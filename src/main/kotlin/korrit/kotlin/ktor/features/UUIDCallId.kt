6edcee379086ad750465636115a0a57322ea8657
Aleksander Stelmaczonek <al.stelmaczonek@gmail.com>
2020-02-06 11:39:29 +0100

    Allow configuring header for UUIDCallId

--- src/main/kotlin/korrit/kotlin/ktor/features/UUIDCallId.kt
+++ src/main/kotlin/korrit/kotlin/ktor/features/UUIDCallId.kt
@@ -9,22 +9,31 @@ import java.util.UUID
 /**
  * This is [CallId] feature with predefined configuration.
  *
- * Call id header: X-Request-ID
+ * Call id header: X-Correlation-ID by default
  * Call id generation: UUID
  * Call id required: yes
  */
 class UUIDCallId {
 
+    /** Configuration of UUIDCallId feature. */
+    class Config {
+        /** Configures which header should be checked for Call Id value. */
+        var header: String = HttpHeaders.XCorrelationId
+    }
+
     /**
      * Feature installation object.
      */
-    companion object Feature : ApplicationFeature<ApplicationCallPipeline, Unit, CallId> {
+    companion object Feature : ApplicationFeature<ApplicationCallPipeline, Config, CallId> {
 
         override val key = CallId.key
 
-        override fun install(pipeline: ApplicationCallPipeline, configure: Unit.() -> Unit): CallId {
+        override fun install(pipeline: ApplicationCallPipeline, configure: Config.() -> Unit): CallId {
+            val config = Config()
+            config.configure()
+
             return CallId.install(pipeline) {
-                header(HttpHeaders.XRequestId)
+                header(config.header)
                 generate { UUID.randomUUID().toString() }
                 verify { it.isNotBlank() }
             }
